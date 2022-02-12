9ecb8cc078bc9c4c8d338b0c0936efc6fedadb19
whoamins <40am1ns@protonmail.com>
2022-02-12 22:08:52 +0300

    init

---
+++ honeypot.ps1
@@ -0,0 +1,11 @@
+﻿$name = "svc_backup"
+$sam = $name -replace ' ', '.'
+$password = "ecfd1f2055b07c306ed3bc2709b5de6!8"
+$serviceName = "BackupServicezxc"
+
+
+New-ADUser -Name $name -SamAccountName $sam -AccountPassword(ConvertTo-SecureString $password -AsPlainText -force) -Enabled $true
+New-Service -Name "$serviceName" -BinaryPathName "C:\Windows\System32\cmd.exe" -DisplayName ″$serviceName″
+setspn -S "HOST/$serviceName" $name
+
+# TODO: Monitor logs with id 4769 and service $serviceName
