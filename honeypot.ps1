9d220cd839969e16c22a384c1718e455cae48274
whoamins <40am1ns@protonmail.com>
2022-02-13 17:59:44 +0300

    Add log monitoring command

--- honeypot.ps1
+++ honeypot.ps1
@@ -1,11 +1,13 @@
 ﻿$name = "svc_backup"
-$sam = $name -replace ' ', '.'
+$sam = $name -replace ' ', '.' # Change this for your company account naming
 $password = "ecfd1f2055b07c306ed3bc2709b5de6!8"
-$serviceName = "BackupServicezxc"
+$serviceName = "BackupService2"
 
 
 New-ADUser -Name $name -SamAccountName $sam -AccountPassword(ConvertTo-SecureString $password -AsPlainText -force) -Enabled $true
 New-Service -Name "$serviceName" -BinaryPathName "C:\Windows\System32\cmd.exe" -DisplayName ″$serviceName″
 setspn -S "HOST/$serviceName" $name
 
-# TODO: Monitor logs with id 4769 and service $serviceName
+Get-EventLog -LogName Security -After (date).AddSeconds(-300) -Before (date) | Where-Object -Property InstanceId -Match "4769" | Where-Object -Property ReplacementStrings -Contains $name | fl
+
+# TODO: Popup if output from Get-EventLog is not null
