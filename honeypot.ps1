8df2935a94ff096d8a296a7775967490adbd42a7
whoam1ns3 <40am1ns@protonmail.com>
2022-02-14 09:52:21 +0300

    Update honeypot.ps1

--- honeypot.ps1
+++ honeypot.ps1
@@ -4,7 +4,7 @@ $password = "ecfd1f2055b07c306ed3bc2709b5de6!8"
 $serviceName = "BackupService"
 
 New-ADUser -Name $name -SamAccountName $sam -AccountPassword(ConvertTo-SecureString $password -AsPlainText -force) -Enabled $true
-New-Service -Name "$serviceName" -BinaryPathName "C:\Windows\System32\calc.exe" -DisplayName ″$serviceName″
+New-Service -Name "$serviceName" -BinaryPathName "C:\Windows\System32\calc.exe" -DisplayName "$serviceName"
 setspn -S "HOST/$serviceName" $name
 
 Get-EventLog -LogName Security -After (date).AddSeconds(-300) -Before (date) | Where-Object -Property InstanceId -Match "4769" | Where-Object -Property ReplacementStrings -Contains $name | fl
