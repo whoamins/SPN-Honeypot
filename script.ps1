4f648bc23354c5f6de9ed4db5f54cd8f8a0cdd7c
whoamins <40am1ns@github.com>
2022-03-07 17:53:08 +0300

    full

---
+++ script.ps1
@@ -0,0 +1,18 @@
+﻿$name = 'svc_backup'
+$command = Get-EventLog -LogName Security -After (date).AddSeconds(-300) -Before (date) | Where-Object -Property InstanceId -Match "4769" | Where-Object -Property ReplacementStrings -Contains $name | fl
+
+
+If ($command -ne $null)  {
+    Add-Type -AssemblyName System.Windows.Forms 
+    $global:balloon = New-Object System.Windows.Forms.NotifyIcon
+    $path = (Get-Process -id $pid).Path
+    $balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path) 
+    $balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning 
+    $balloon.BalloonTipText = 'SPN Honeypot has been triggered!'
+    $balloon.BalloonTipTitle = "Kerberoasting Detected!" 
+    $balloon.Visible = $true 
+    $balloon.ShowBalloonTip(5000)
+}
+Else {
+    echo "Null"
+}
---
+++ sheduler.ps1
@@ -0,0 +1,5 @@
+﻿$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-File C:\Users\Administrator\Desktop\script.ps1'
+
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5)
+
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "BackupTask"
