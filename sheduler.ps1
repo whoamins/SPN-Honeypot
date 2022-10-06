3d3bdf984af2162fcd589fc2eb35246742f98b1d
whoam1ns3 <40am1ns@protonmail.com>
2022-10-06 23:20:59 +0300

    Delete sheduler.ps1

--- sheduler.ps1
+++
@@ -1,5 +0,0 @@
-﻿$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-File C:\Users\Administrator\Desktop\script.ps1'
-
-$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5)
-
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "BackupTask"
